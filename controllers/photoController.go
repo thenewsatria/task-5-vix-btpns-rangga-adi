fe1b3d9f11856843c1a27d8c2628f6581679518c
thenewsatria <satriabelajarterus@gmail.com>
2023-05-28 18:52:31 +0700

    readable error message photoController

--- controllers/photoController.go
+++ controllers/photoController.go
@@ -44,7 +44,7 @@ func (photoController *PhotoController) HandleCreatePhoto() gin.HandlerFunc {
 			c.JSON(http.StatusBadRequest, &app.JsendFailResponse{
 				Status: "fail",
 				Data: gin.H{
-					"form-data": err.Error(),
+					"form-data": "Invalid form-data format",
 				},
 			})
 			return
@@ -171,7 +171,7 @@ func (photoController *PhotoController) HandleUpdatePhoto() gin.HandlerFunc {
 			c.JSON(http.StatusBadRequest, &app.JsendFailResponse{
 				Status: "fail",
 				Data: gin.H{
-					"form-test": err.Error(),
+					"form-data": "Invalid form-data format",
 				},
 			})
 			return
